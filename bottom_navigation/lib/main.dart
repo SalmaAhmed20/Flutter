4e5b3da4305b7f0d614502a4077523b58903ae53
SalmaAhmed20 <64385957+SalmaAhmed20@users.noreply.github.com>
2021-08-06 01:08:35 +0200

    samplecode

---
+++ bottom_navigation/lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'main2.dart';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({Key key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _currentIndex=0;
+  List _screens=[PlaceholderWidget(Colors.white),
+    PlaceholderWidget(Colors.deepOrange),
+    PlaceholderWidget(Colors.green),PlaceholderWidget(Colors.black87)];
+
+  void _updateIndex(int value) {
+    setState(() {
+      _currentIndex = value;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Bottom Navigation Bar"),
+      ),
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _currentIndex,
+        onTap: _updateIndex,
+        selectedItemColor: Colors.blue[700],
+        selectedFontSize: 13,
+        unselectedFontSize: 13,
+        iconSize: 30,
+        items: [
+          BottomNavigationBarItem(
+            label: "Home",
+            icon: Icon(Icons.home),
+
+          ),
+          BottomNavigationBarItem(
+            label: "Search",
+            icon: Icon(Icons.search),
+          ),
+          BottomNavigationBarItem(
+            label: "Categories",
+            icon: Icon(Icons.grid_view),
+          ),
+          BottomNavigationBarItem(
+            label: "My Account",
+            icon: Icon(Icons.account_circle_outlined),
+          ),
+        ],
+      ),
+    );
+  }
+
+}
